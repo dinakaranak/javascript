9ff8e7c20aed0b86e114c82f278532e72c85b72e
dinakaranak <dinakaranak001@gmail.com>
2025-01-25 09:34:41 +0530

    change something

--- DateObj.js
+++ DateObj.js
@@ -22,14 +22,65 @@ console.log(currentDate.getDay());
 // difference b/w 2 dates
 // age calculator
 
+// formate the current Date and time
+
+function objectDate(){
+let dateObj = new Date();
+let formattedDateTime = dateObj.toLocaleString();
+console.log(formattedDateTime);
+}
+objectDate()
 
 
 
+//compare 2 dates and print which is latest date
 
+function compareToDate(){
+  
+    let date1 = new Date("2024-02-25")
+    let date2= new Date("2023-09-26")
+    if(date1>date2){
+        console.log(date1.toLocaleDateString() ,'is the latest date');
+    }
+    else if(date1<date2){
+        console.log(date2.toLocaleDateString(),'is the latest date');    
+    }
+    else{
+        console.log("both are equal");
+        
+    }
+}
 
+compareToDate()
 
+// difference b/w 2 dates
+function diffBtwn(){
+let date1 = new Date('2025-01-24');
+let date2 = new Date('2024-01-28');
+let diffMs = date1 - date2;
+let diffSeconds = Math.floor(diffMs / 1000);
+let diffMinutes = Math.floor(diffSeconds / 60);
+let diffHours = Math.floor(diffMinutes / 60);
+let diffDays = Math.floor(diffHours / 24);
+console.log(diffDays,"Days");
+}
+diffBtwn()
 
-
+// age calculator
+function ageCalc(){
+  
+    let birthDate=new Date("2004-01-21")
+    let currentDate=new Date()
+    let age=currentDate.getFullYear()-birthDate.getFullYear();
+    let month=currentDate.getMonth()-birthDate.getMonth();
+    let day=currentDate.getDate()-birthDate.getDate()
+    if(month<0 || (month===0 && day<0)){ 
+        age--;
+    }
+    
+    console.log(age,"age");                                                                                                                                                                                                            
+}  
+ageCalc()
 
 
 
